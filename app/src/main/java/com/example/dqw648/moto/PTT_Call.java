f37df644ab694cbcb38fe874b5a881e7c3d08c86
Shu Yang Chew <PMXT36@motorolasolutions.com>
2017-09-19 16:10:50 +0800

    refactor api name and add implementations, not tested

--- app/src/main/java/com/example/dqw648/moto/PTT_Call.java
+++ app/src/main/java/com/example/dqw648/moto/PTT_Call.java
@@ -2,11 +2,22 @@ package com.example.dqw648.moto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import com.zello.sdk.Zello;
 
 public class PTT_Call extends AppCompatActivity {
 
     //String
     String detected_username;
+    String tag = "zello";
+
+    /*
+    *
+    * Sample to broadcast to everyone
+    * Zello.getInstance().setSelectedChannelOrGroup("Everyone");
+    *
+    * */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,15 +26,24 @@ public class PTT_Call extends AppCompatActivity {
         detected_username = from_listview.getString("username");
     }
 
-    public void ptt_call(){
-
+    public void ptt_init(){
+        Zello.getInstance().configure("net.loudtalks", this);
     }
 
-    public void ptt_start(){
+    public void ptt_start(String name){
+        if (name != "") {
+            Zello.getInstance().setSelectedUserOrGateway(name);
+            Log.d(tag, String.format("start ptt call to %s\r\n", name));
+            Zello.getInstance().beginMessage();
+        }
+        else{
+            Log.e(tag, "username is empty");
+        }
 
     }
 
     public void ptt_stop(){
-
+        Zello.getInstance().endMessage();
+        Log.d(tag, "stop ptt call\r\n");
     }
 }
