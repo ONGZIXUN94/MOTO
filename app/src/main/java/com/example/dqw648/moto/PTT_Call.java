f80c691671fb1da773b7ebbd03385b4182d1fb67
Shu Yang Chew <PMXT36@motorolasolutions.com>
2017-09-19 16:24:28 +0800

    hook up pttcall button and event handler to ptt call api

--- app/src/main/java/com/example/dqw648/moto/PTT_Call.java
+++ app/src/main/java/com/example/dqw648/moto/PTT_Call.java
@@ -3,6 +3,11 @@ package com.example.dqw648.moto;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.zello.sdk.Zello;
 
 public class PTT_Call extends AppCompatActivity {
@@ -22,8 +27,31 @@ public class PTT_Call extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ptt__call);
+
+        ptt_init();
+
         Bundle from_listview = getIntent().getExtras();
         detected_username = from_listview.getString("username");
+
+        Button pttButton = (Button) findViewById(R.id.pttButton);
+
+        pttButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                int action = motionEvent.getAction();
+                if (action == MotionEvent. ACTION_DOWN ) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Transmitting...", Toast.LENGTH_SHORT);
+                    toast.show();
+                    ptt_start(detected_username);
+                } else if (action == MotionEvent. ACTION_UP || action == MotionEvent. ACTION_CANCEL ) {
+                    ptt_stop();
+                    Toast toast = Toast.makeText(getApplicationContext(), "Voice message sent.", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                return false;
+            }
+        });
     }
 
     public void ptt_init(){
