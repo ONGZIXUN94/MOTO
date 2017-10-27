835b1d685dc7e828341a2a2242410f352bae2884
Shu Yang Chew <PMXT36@motorolasolutions.com>
2017-10-27 18:10:38 +0800

    extract all direct call to zello sdk from activity into ZelloWrapper class, added getThisUserName() method that will return the username that is signed in on ZelloWork app, also added group call faeture, application should call configure call first before call ptt_start, expect user call configure on mainactivity and then call ptt_start and end on pttActivity

--- app/src/main/java/com/example/dqw648/moto/PTT_Call.java
+++ app/src/main/java/com/example/dqw648/moto/PTT_Call.java
@@ -2,20 +2,16 @@ package com.example.dqw648.moto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.zello.sdk.Zello;
-
 public class PTT_Call extends AppCompatActivity {
 
     //String
     String detected_username;
-    String tag = "zello";
 
     /*
     * NOTE: You need to have zello work app (not the one from appstore) installed using this link
@@ -32,7 +28,8 @@ public class PTT_Call extends AppCompatActivity {
         setContentView(R.layout.activity_ptt__call);
 
         TextView tv_callname = (TextView) findViewById(R.id.tv_callname);
-        ptt_init();
+        ZelloWrapper.init(this);
+        String dummy = ZelloWrapper.getThisUserName();
 
         Bundle from_listview = getIntent().getExtras();
         detected_username = from_listview.getString("username");
@@ -49,35 +46,14 @@ public class PTT_Call extends AppCompatActivity {
                 if (action == MotionEvent. ACTION_DOWN ) {
                     Toast toast = Toast.makeText(getApplicationContext(), "Transmitting...", Toast.LENGTH_SHORT);
                     toast.show();
-                    ptt_start(detected_username);
+                    ZelloWrapper.ptt_start();
                 } else if (action == MotionEvent. ACTION_UP || action == MotionEvent. ACTION_CANCEL ) {
-                    ptt_stop();
+                    ZelloWrapper.ptt_stop();
                     Toast toast = Toast.makeText(getApplicationContext(), "Voice message sent.", Toast.LENGTH_SHORT);
                     toast.show();
                 }
                 return false;
             }
         });
-    }
-
-    public void ptt_init(){
-        Zello.getInstance().configure("net.loudtalks", this);
-    }
-
-    public void ptt_start(String name){
-        if (name != "") {
-            Zello.getInstance().setSelectedUserOrGateway(name);
-            Log.d(tag, String.format("start ptt call to %s\r\n", name));
-            Zello.getInstance().beginMessage();
-        }
-        else{
-            Log.e(tag, "username is empty");
-        }
-
-    }
-
-    public void ptt_stop(){
-        Zello.getInstance().endMessage();
-        Log.d(tag, "stop ptt call\r\n");
     }
 }
